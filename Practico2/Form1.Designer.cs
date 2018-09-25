d1c0eb9bae017b780a4cc0d5b70ecbfea04fecbc
IrraSeba <sebastianirrazabal10@hotmail.com>
2018-09-25 16:16:55 -0300

    Commit de prueba

---
+++ Practico2/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace Practico2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblContConsonantes = new System.Windows.Forms.Label();
+            this.lblContVocales = new System.Windows.Forms.Label();
+            this.lblContLineas = new System.Windows.Forms.Label();
+            this.lblContTabulador = new System.Windows.Forms.Label();
+            this.lblContEspBlanco = new System.Windows.Forms.Label();
+            this.lblContPalabras = new System.Windows.Forms.Label();
+            this.lblContCaracteres = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnGuardarComo = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblContConsonantes);
+            this.groupBox1.Controls.Add(this.lblContVocales);
+            this.groupBox1.Controls.Add(this.lblContLineas);
+            this.groupBox1.Controls.Add(this.lblContTabulador);
+            this.groupBox1.Controls.Add(this.lblContEspBlanco);
+            this.groupBox1.Controls.Add(this.lblContPalabras);
+            this.groupBox1.Controls.Add(this.lblContCaracteres);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.btnGuardarComo);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(514, 504);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // lblContConsonantes
+            // 
+            this.lblContConsonantes.AutoSize = true;
+            this.lblContConsonantes.Location = new System.Drawing.Point(346, 409);
+            this.lblContConsonantes.Name = "lblContConsonantes";
+            this.lblContConsonantes.Size = new System.Drawing.Size(13, 13);
+            this.lblContConsonantes.TabIndex = 28;
+            this.lblContConsonantes.Text = "0";
+            // 
+            // lblContVocales
+            // 
+            this.lblContVocales.AutoSize = true;
+            this.lblContVocales.Location = new System.Drawing.Point(346, 373);
+            this.lblContVocales.Name = "lblContVocales";
+            this.lblContVocales.Size = new System.Drawing.Size(13, 13);
+            this.lblContVocales.TabIndex = 27;
+            this.lblContVocales.Text = "0";
+            // 
+            // lblContLineas
+            // 
+            this.lblContLineas.AutoSize = true;
+            this.lblContLineas.Location = new System.Drawing.Point(346, 339);
+            this.lblContLineas.Name = "lblContLineas";
+            this.lblContLineas.Size = new System.Drawing.Size(13, 13);
+            this.lblContLineas.TabIndex = 26;
+            this.lblContLineas.Text = "0";
+            // 
+            // lblContTabulador
+            // 
+            this.lblContTabulador.AutoSize = true;
+            this.lblContTabulador.Location = new System.Drawing.Point(346, 302);
+            this.lblContTabulador.Name = "lblContTabulador";
+            this.lblContTabulador.Size = new System.Drawing.Size(13, 13);
+            this.lblContTabulador.TabIndex = 25;
+            this.lblContTabulador.Text = "0";
+            // 
+            // lblContEspBlanco
+            // 
+            this.lblContEspBlanco.AutoSize = true;
+            this.lblContEspBlanco.Location = new System.Drawing.Point(346, 263);
+            this.lblContEspBlanco.Name = "lblContEspBlanco";
+            this.lblContEspBlanco.Size = new System.Drawing.Size(13, 13);
+            this.lblContEspBlanco.TabIndex = 24;
+            this.lblContEspBlanco.Text = "0";
+            // 
+            // lblContPalabras
+            // 
+            this.lblContPalabras.AutoSize = true;
+            this.lblContPalabras.Location = new System.Drawing.Point(346, 196);
+            this.lblContPalabras.Name = "lblContPalabras";
+            this.lblContPalabras.Size = new System.Drawing.Size(13, 13);
+            this.lblContPalabras.TabIndex = 23;
+            this.lblContPalabras.Text = "0";
+            // 
+            // lblContCaracteres
+            // 
+            this.lblContCaracteres.AutoSize = true;
+            this.lblContCaracteres.Location = new System.Drawing.Point(346, 230);
+            this.lblContCaracteres.Name = "lblContCaracteres";
+            this.lblContCaracteres.Size = new System.Drawing.Size(13, 13);
+            this.lblContCaracteres.TabIndex = 22;
+            this.lblContCaracteres.Text = "0";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(247, 456);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Guardar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardarComo
+            // 
+            this.btnGuardarComo.Location = new System.Drawing.Point(364, 456);
+            this.btnGuardarComo.Name = "btnGuardarComo";
+            this.btnGuardarComo.Size = new System.Drawing.Size(87, 23);
+            this.btnGuardarComo.TabIndex = 13;
+            this.btnGuardarComo.Text = "Guardar como";
+            this.btnGuardarComo.UseVisualStyleBackColor = true;
+            this.btnGuardarComo.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Caracteres",
+            "Espacios en blanco",
+            "Tabuladores",
+            "Lineas",
+            "Vocales",
+            "Consonantes"});
+            this.comboBox1.Location = new System.Drawing.Point(92, 458);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(89, 409);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(72, 13);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Consonantes:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(89, 373);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Vocales:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(89, 339);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Lineas:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(89, 302);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Tabuladores:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(89, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Espacios en blanco:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(89, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Caracteres:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(89, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Palabras:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(92, 61);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(359, 111);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(267, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(376, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Examinar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 461);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Quitar:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Archivo:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 528);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnGuardarComo;
+        private System.Windows.Forms.Label lblContCaracteres;
+        private System.Windows.Forms.Label lblContConsonantes;
+        private System.Windows.Forms.Label lblContVocales;
+        private System.Windows.Forms.Label lblContLineas;
+        private System.Windows.Forms.Label lblContTabulador;
+        private System.Windows.Forms.Label lblContEspBlanco;
+        private System.Windows.Forms.Label lblContPalabras;
+    }
+}
+
---
+++ Practico2/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Practico2
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog abrir = new OpenFileDialog();
+
+        int a, e, i, o, u;
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        String cadena;
+        int cont = 0;
+        int cont_palabras = 0;
+        int cont_letras = 0;
+        int cont_vocales = 0;
+        int cont_consonantes = 0;
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        int cont_Eblancos = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (abrir.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+
+                abrir.Title = "Abrir archivo";
+
+                textBox1.Text = abrir.FileName;
+                StreamReader read = new StreamReader(File.OpenRead(abrir.FileName));
+
+                textBox2.Text = read.ReadToEnd();
+                abrir.Dispose();
+                MessageBox.Show("Archivo cargado .... Acepta para continuar !");
+                textBox2.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            SaveFileDialog save = new SaveFileDialog();
+
+            if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                StreamWriter write = new StreamWriter(File.Create(save.FileName));
+
+                write.Write(textBox2.Text);
+                write.Dispose();
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+            //System.String[] = textBox2.Text;
+            lblContPalabras.Text = " " + textBox2.Text.Length;
+            cont_palabras++;
+
+            lblContCaracteres.Text = "" + textBox2.Text.Length;// ok
+
+            lblContEspBlanco.Text = "" + textBox2.Text.Length;
+
+            lblContTabulador.Text = "" + textBox2.Text.Length;
+
+            lblContLineas.Text = "" + textBox2.Text.Length;
+
+            lblContVocales.Text = "" + textBox2.Text.Length;
+            
+            //cadena = textBox2.Text;
+
+            //for (int x =0; x < cadena.Length; x++) {
+            //    if (cadena[x] == 'a' || cadena[x] == 'A')
+            //        a++;
+            //    if (cadena[x] == 'e' || cadena[x] == 'E')
+            //        a++;
+            //    if (cadena[x] == 'i' || cadena[x] == 'I')
+            //        a++;
+            //    if (cadena[x] == 'o' || cadena[x] == 'O')
+            //        a++;
+            //    if (cadena[x] == 'u' || cadena[x] == 'U')
+            //        a++;
+            //    {
+            //    }
+            //    textBox2.Text = a.ToString();
+            //    textBox2.Text = e.ToString();
+            //    textBox2.Text = i.ToString();
+            //    textBox2.Text = o.ToString();
+            //    textBox2.Text = u.ToString();
+
+            
+            lblContConsonantes.Text = "" + textBox2.Text.Length;
+        }
+
+
+        
+        }
+    }
